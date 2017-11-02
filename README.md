d2d23d5eeb7688973b176e5569d7d93bfb6eda5d
Tengfei Wang <tengfeiwang@skspruce.net>
2017-11-02 15:19:55 +0800

    hdfs简单读写文件API

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bigdata
 基于大数据平台各组件的基本操作
+目前包含HDFS文件读写，在子MODULE hadoop中
+
+
+后续会持续更新
---
+++ hadoop/src/main/java/com/mutou/bigdata/hdfs/TestHDFS.java
@@ -0,0 +1,60 @@
+package com.mutou.bigdata.hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created By sk-tengfeiwang on 2017/11/2.
+ */
+public class TestHDFS {
+    private static String dir = "/tmp/testhdfsdir";
+    private static String targetFile = "/write_test_file";
+    private static Configuration conf = new Configuration();
+
+    static {
+        conf.set("fs.default.name", "hdfs://spark183:9000");
+    }
+
+    public static void main(String[] args) throws Exception {
+        writeFile();
+        readFile();
+    }
+
+    public static void writeFile() throws Exception {
+        FileSystem fs = FileSystem.get(conf);
+
+        //创建目录
+        Path tmpPath = new Path(dir);
+        if (!fs.exists(tmpPath)) {
+            fs.mkdirs(tmpPath);
+        }
+
+        FSDataOutputStream outputStream = fs.create(new Path(dir + targetFile), true);
+
+        for (int i = 0; i < 10; i++) {
+            outputStream.writeBytes("the first hdfs write file" + i + "\n");
+        }
+        outputStream.close();
+    }
+
+    public static void readFile() throws Exception {
+        FileSystem fs = FileSystem.get(conf);
+
+        Path path = new Path(dir + targetFile);
+        FSDataInputStream inputStream = fs.open(path);
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(inputStream));
+        String info = null;
+        while ((info = bf.readLine()) != null) {
+            System.out.println(info);
+        }
+
+        inputStream.close();
+    }
+}
